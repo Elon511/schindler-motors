6275dd00a43e9956e2b30577a0c1c82fe6a87150
Elon511 <316717235+Elon511@users.noreply.github.com>
2026-08-18 20:33:08 +0400

    Add Schindler sales email

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ window.SITE_CONFIG = {
   landingId: "schindler-motors",
   phone: "+1 (814) 933-3155",
   phones: ["+1 (814) 933-3155", "+1 (814) 400-9102"],
+  email: "Sales@schindlermotors.com",
   address: "102 E Plank Rd, Port Matilda, PA 16870",
   leadEndpoint: "/api/leads",
   liveChatLicense: "",
